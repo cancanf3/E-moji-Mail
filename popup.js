43d5ef69a9033d6c19928929e8e0e65b0d806e65
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2017-02-25 14:43:13 -0500

    jesus christ saved us

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
-let body = gmail.dom.email_contents();
 
-var body_text = $(body).text();
 
-$("#checkPage").on("click","#checkPage", function() {
+
+//const GmailFactory = require("gmail-js");
+//const gmail = new GmailFactory.Gmail($);
+
+console.log('hello!');
+
+$(document).ready(function(){
+	let gmail = new Gmail();
+	let body = gmail.dom.email_contents();
+	console.log(body);
+
+	let email = gmail.get.user_email();
+	console.log(email);
+
+	var body_text = $(body).text();
 	console.log(body_text);
 
+	$("#checkPage").on("click", function() {
+		console.log(body_text);
+
+	});
 });
