43c88cbe70f59b8e5cb9ec19b4f756b22cba9e28
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2017-02-25 19:05:40 -0500

    Setting up everythin to implement Ajax

--- popup.js
+++ popup.js
@@ -2,13 +2,13 @@
  * Author: Jose Peña, Navon Francis, Tim Henry, Alexander Lascaibar
  * Hackathon@FIU: Mango Hacks  
  * 02/24/17 - 02/26/17
- * Google Extension (JavaScript + IBM Watson API)
+ * Google Extension (JavaScript + Java + IBM Watson API )
  */
 
 
 
 
-// Back-end is waiting for a trigger from the extension to Analyze the text.
+// Creating a Json package to comunicate with the server is waiting for a trigger from the extension to Analyze the text.
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     if (request.analyze == true) {
     	//Here is where the server logic should be
